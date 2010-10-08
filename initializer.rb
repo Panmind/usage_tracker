021c3198884fd5e35963872ed36815be8e71d622
Christian Wörner <christianworner@gmail.com>
2010-10-08 22:07:36 +0200

    adds and mods some basic couchdb views

--- initializer.rb
+++ initializer.rb
@@ -15,10 +15,10 @@ module UsageTrackerSetup
             }
           ]                                                        
         },
-        'by_timestamp_and_user' => {
+        'by_user_and_timestamp' => {
           'map' => %[
             function (doc) {                                        
-              emit ([doc._id, doc.user_id], doc);                                                    
+              emit ([doc.user_id, doc._id], doc);                                                    
             }
           ]
         },
@@ -50,8 +50,71 @@ module UsageTrackerSetup
 	         return sum(values);
 	      }
           ]
-          }
-	}
+          },
+	  'user_res_count' => {
+	      'map' =>%[ 
+	      	 function(doc) {
+	    	   if (doc.request_path.indexOf("res/") != -1) {
+                     emit([doc.user_id, doc.request_path.split("/")[2]], 1);
+                   }
+                 }
+	      ],
+	      'reduce' => %[
+	        function(keys, values,rereduce) {
+	         return sum(values);
+	       }
+              ]
+	   },
+	  'user_area_count' => {
+	      'map' =>%[ 
+	      	 function(doc) {
+	    	   if (doc.request_path.indexOf("res/") != -1) {
+                   } else if (doc.request_path.indexOf("projects/") != -1) {
+                     area = "projects"
+                   } else if (doc.request_path.indexOf("users/") != -1) {
+                     area = "users"
+                   } else if (doc.request_path.indexOf("account/") != -1) {
+                     area = "account"
+                   } else if (doc.request_path.indexOf("publish/") != -1) {
+                     area = "publish"
+		   } else if (doc.request_path.indexOf("search/") != -1) {
+                     area = "search"
+		   } 
+		   emit([doc.user_id, area], 1);
+                 }
+	      ],
+	      'reduce' => %[
+	        function(keys, values,rereduce) {
+	         return sum(values);
+	       }
+              ]
+	   },
+	  'area_count' => {
+	      'map' =>%[ 
+	      	 function(doc) {
+	    	   if (doc.request_path.indexOf("res/") != -1) {
+                     area = "res"
+                   } else if (doc.request_path.indexOf("projects/") != -1) {
+                     area = "projects"
+                   } else if (doc.request_path.indexOf("users/") != -1) {
+                     area = "users"
+                   } else if (doc.request_path.indexOf("account/") != -1) {
+                     area = "account"
+                   } else if (doc.request_path.indexOf("publish/") != -1) {
+                     area = "publish"
+		   } else if (doc.request_path.indexOf("search/") != -1) {
+                     area = "search"
+		   } 
+		   emit(area, 1);
+                 }
+	      ],
+	      'reduce' => %[
+	        function(keys, values,rereduce) {
+	         return sum(values);
+	       }
+              ]
+	    }
+	 }
       })
     end
   end
