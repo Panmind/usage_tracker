bb8318adb465cb953b14e79c31aa5dbb195d1818
Christian Wörner <christianworner@gmail.com>
2010-10-08 21:19:23 +0200

    adds some preliminary couchdb view-functions

--- initializer.rb
+++ initializer.rb
@@ -2,11 +2,11 @@ module UsageTrackerSetup
   def UsageTrackerSetup.init()
     puts "db-setup"
     db = CouchRest.database!("localhost:5984/pm_usage")
-    puts "re-initializing"
     view = db.get('_design/basic') rescue nil
     if view.nil?
       db.save_doc({
       '_id'   => '_design/basic',
+      'language' => "javascript",
       'views' => {
         'by_user_and_timestamp' => {
           'map' => %[
@@ -21,8 +21,37 @@ module UsageTrackerSetup
               emit ([doc._id, doc.user_id], doc);                                                    
             }
           ]
-        }
-      }
+        },
+        'res_count' => {
+	  'map' => %[
+	    function(doc) {
+	  	if (doc.request_path.indexOf("res/") != -1){
+                    emit(doc.request_path.split("/")[2], 1);
+		}
+            }
+          ],
+	  'reduce' => %[
+	      function(keys, values,rereduce) {
+	         return sum(values);
+	      }
+	  ]
+          },
+	  'res_item_count' => {
+	      'map' =>%[ 
+	      	 function(doc) {
+	    	   if (doc.request_path.indexOf("res/") != -1 & 
+		       doc.request_path.split("/").length > 3) {
+                     emit([doc.request_path.split("/")[2],doc.request_path.split("/")[3]], 1);
+                   }
+                 }
+	      ],
+	  'reduce' => %[
+	      function(keys, values,rereduce) {
+	         return sum(values);
+	      }
+          ]
+          }
+	}
       })
     end
   end
