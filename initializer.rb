7efe04164e3f01e93c2340a227ce412981df2e71
Marcello Barnaba <vjt@openssl.it>
2010-10-22 14:15:08 +0200

    UT: DRYed up area inferring by pre-processing the YAML views file with ERB

--- initializer.rb
+++ initializer.rb
@@ -1,12 +1,11 @@
+require 'erb'
+
 module UsageTrackerSetup
   ## this function assures that the required couchdb DB is up and running and has a set of basic queries already registered.
   def UsageTrackerSetup.init(couchdb_url)
-    puts "db-setup"
     db = CouchRest.database!(couchdb_url) #FIXME
-    begin
-      db.get('_design/basic')
-    rescue
-      db.save_doc(YAML.load_file('extras/usage_tracker/views.yml'))
-    end
+    db.get('_design/basic')
+  rescue RestClient::ResourceNotFound
+    db.save_doc YAML.load(ERB.new(File.read('extras/usage_tracker/views.yml')).result)
   end
 end
