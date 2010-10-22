90f7eaebb11f9b770e7af9bd70aef92a130ef2f7
Marcello Barnaba <vjt@openssl.it>
2010-10-22 14:29:57 +0200

    UT: added versioning to the design document

--- initializer.rb
+++ initializer.rb
@@ -4,8 +4,20 @@ module UsageTrackerSetup
   ## this function assures that the required couchdb DB is up and running and has a set of basic queries already registered.
   def UsageTrackerSetup.init(couchdb_url)
     db = CouchRest.database!(couchdb_url) #FIXME
-    db.get('_design/basic')
-  rescue RestClient::ResourceNotFound
-    db.save_doc YAML.load(ERB.new(File.read('extras/usage_tracker/views.yml')).result)
+
+    new = YAML.load(ERB.new(File.read('extras/usage_tracker/views.yml')).result)
+    id  = new['_id']
+    old = db.get id rescue nil
+
+    if old.nil?
+      puts "** Creating Design Document #{id} v#{new['version']}"
+      db.save_doc new
+
+    elsif old['version'].to_i < new['version'].to_i
+      puts "** Upgrading Design Document #{id} to v#{new['version']}"
+      db.delete_doc old
+      db.save_doc new
+    end
+
   end
 end
