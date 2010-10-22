20abc2cbbc65c0843b2045b6fc20adbf0ca439a1
Marcello Barnaba <vjt@openssl.it>
2010-10-22 15:45:15 +0200

    UT: handle configuration errors, added a singleton database instance into UsageTracker.database

--- initializer.rb
+++ initializer.rb
@@ -1,23 +1,80 @@
 require 'erb'
+require 'pathname'
+require 'ostruct'
 
-module UsageTrackerSetup
-  ## this function assures that the required couchdb DB is up and running and has a set of basic queries already registered.
-  def UsageTrackerSetup.init(couchdb_url)
-    db = CouchRest.database!(couchdb_url) #FIXME
-
-    new = YAML.load(ERB.new(File.read('extras/usage_tracker/views.yml')).result)
-    id  = new['_id']
-    old = db.get id rescue nil
-
-    if old.nil?
-      puts "** Creating Design Document #{id} v#{new['version']}"
-      db.save_doc new
-
-    elsif old['version'].to_i < new['version'].to_i
-      puts "** Upgrading Design Document #{id} to v#{new['version']}"
-      db.delete_doc old
-      db.save_doc new
+module UsageTracker
+  class << self
+    def log(message)
+      puts "** UT: #{message}"
     end
 
+    # Memoizes the current environment
+    def env
+      @env ||= ENV['RAILS_ENV'] || ARGV[0] || 'development'
+    end
+
+    # Memoizes settings from ../../../config/settings.yml config,
+    # relative from __FILE__ and searches for the "usage_tracker"
+    # configuration block. Raises RuntimeError if it cannot find
+    # the configuration.
+    #
+    def settings
+      @settings ||= begin
+        log "Loading #{env} environment"
+
+        rc_file = Pathname.new(__FILE__).join('..', '..', '..', 'config', 'settings.yml')
+        raise "Configuration file #{rc_file} not found" unless rc_file.exist?
+
+        settings = YAML.load_file(rc_file.to_s)[env][:usage_tracker]
+
+        unless settings
+          raise ":usage_tracker configuration block not found in #{rc_file}"
+        end
+
+        OpenStruct.new settings
+      end
+    end
+
+    def database
+      @database or raise "Not connected to the database"
+    end
+
+    # Connects to the configured CouchDB and memoizes the
+    # CouchRest::Database connection into an instance variable
+    # and calls +load_views!+
+    #
+    # Raises RuntimeError if the connection could not be established
+    #
+    def connect!
+      @database =
+        CouchRest.database!(settings.couchdb).tap do |db|
+          db.info
+          log "Connected to #{settings.couchdb}"
+        end
+
+      load_views!
+    rescue Errno::ECONNREFUSED
+      raise "Unable to connect to #{settings.couchdb}"
+    end
+
+    private
+      # Loads CouchDB views from views.yml and verifies that
+      # they are loaded in the current instance, upgrading
+      # them if necessary.
+      def load_views!
+        new = YAML.load ERB.new(File.read('extras/usage_tracker/views.yml')).result
+        id  = new['_id']
+        old = database.get id
+
+        if old['version'].to_i < new['version'].to_i
+          log "Upgrading Design Document #{id} to v#{new['version']}"
+          database.delete_doc old
+          database.save_doc new
+        end
+
+      rescue RestClient::ResourceNotFound
+        log "Creating Design Document #{id} v#{new['version']}"
+        database.save_doc new
+      end
   end
 end
--- reactor.rb
+++ reactor.rb
@@ -7,51 +7,31 @@ require 'couchrest'
 require 'extras/usage_tracker/initializer'
 require 'yaml'
 
-module UsageWriter
-    # basic setup
-    ARGV.push "development" unless  ARGV.length > 0
-    SETTINGS =  YAML::load(File.open(File.dirname(__FILE__) + "/../../config/settings.yml"))[ARGV[0]]
-    @couchdb_url = SETTINGS[:usage_tracker][:couchdb]
-    puts "using couchdb database:#{@couchdb_url}"
-    ARGV.push @couchdb_url
-    UsageTrackerSetup.init(@couchdb_url)
-    
-    # this function assures that a global variable for the couchrest database-object is available 
-    # it is called EVERY TIME a new connection is made 
-    #     - given the use of this event-machine reactor as a server for a webapp this means that 
-    #       this function is called on every connection) 
-    def initialize
-      @db ||= CouchRest.database!(ARGV[1])
-    end
-
+module UsageTracker
+  module Reactor
     # this function is called upon every data reception
+    #
     def receive_data(data)
-      d = eval data
+      d = eval data # FIXME SECURITY HOLE
       # timestamp as _id has the advantage that documents are sorted automatically by couchdb, 
       # eventual duplication (multiple servers) of the id are avoided by adding a random string at the end 
       begin
-        begin
-          d["_id"] = Time.now.to_f.to_s.ljust(16,"0")
-          @db.save_doc(d)
-        rescue RestClient::Conflict
-          d["_id"] = Time.now.to_f.to_s.ljust(16,"0") + (0..9).to_a.rand.to_s
-          @db.save_doc(d)
-        end
+        d["_id"] = Time.now.to_f.to_s.ljust(16,"0")
+        UsageTracker.database.save_doc(d)
+      rescue RestClient::Conflict
+        d["_id"] = Time.now.to_f.to_s.ljust(16,"0") + (0..9).to_a.rand.to_s
+        retry
       rescue Encoding::UndefinedConversionError
-        :ok
+        :ok # FIXME handle this error properly
       end
     end
 
 end
 
+UsageTracker.connect!
 
-# the reactor
-EventMachine::run do
-    #puts "debug: #{ARGV.inspect}"
-    env = ARGV.length > 0 ? ARGV[0] : "development"
-    em_url = UsageWriter::SETTINGS[:usage_tracker][:reactor]
-    host = em_url.split(":")[0]
-    port =  em_url.split(":")[1]
-    EventMachine::start_server host, port, UsageWriter
-    puts "Started UsageWriter on #{host}:#{port}..."
+EventMachine.run do
+  host, port = UsageTracker.settings.reactor.split(':')
+  EventMachine::start_server host, port, UsageTracker::Reactor
+  UsageTracker.log "Started Reactor on #{host}:#{port}"
 end
