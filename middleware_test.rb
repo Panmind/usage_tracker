5225c44e9c886141f30515e46514d4a08315e5d1
Marcello Barnaba <vjt@openssl.it>
2010-10-25 15:32:06 +0200

    UT: test search results with expectations

--- middleware_test.rb
+++ middleware_test.rb
@@ -23,11 +23,29 @@ module UsageTracker
       end
 
       should "result in a db-entry containing search results" do
+        res, users, assets =
+          mock_search_results_for(Array.new(2) { Factory.create(:res)  }),
+          mock_search_results_for(Array.new(3) { Factory.create(:user) }),
+          mock_search_results_for(Network::AssetsController::ContentModels.map {|name|
+            Array.new(2) { Factory(name.underscore.to_sym).reload.asset } }.flatten)
+
+        Res.stubs(:search).returns(res)
+        User.stubs(:search).returns(users)
+        NetworkAsset.stubs(:search).returns(assets)
+
         get '/search/e'
         assert_response :success
+
         doc = db.view('basic/by_timestamp', :descending => true, :limit => 1).rows.first.value
         assert !doc.context.blank?
-        assert doc.context.results.users.size > 0
+
+        assert_equal 'e', doc.context.query
+        assert_equal [],  doc.context.tags
+        assert_equal nil, doc.context.cat
+
+        assert_equal res.map(&:id),    doc.context.results.res
+        assert_equal users.map(&:id),  doc.context.results.users
+        assert_equal assets.map(&:id), doc.context.results.assets
       end
     end
   end
