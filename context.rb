4616fe97f03a610c58f15181496b7d37dc5c3b12
Marcello Barnaba <vjt@openssl.it>
2010-12-03 18:09:05 +0100

    UT: use request.env instead of plain env when setting Context data

--- context.rb
+++ context.rb
@@ -6,13 +6,13 @@ module UsageTracker
     # Sets the env +Key+ variable with the provided +data+
     #
     def usage_tracker_context=(data)
-      unless env[key].blank?
+      unless request.env[key].blank?
         unless Rails.env.test? && !caller.grep(/test\/functional/).blank?
           UsageTracker.log 'WARNING: overwriting context data!'
         end
       end
 
-      env[key] = data
+      request.env[key] = data
     end
 
     # Shorthand for self.usage_tracker_context = data
