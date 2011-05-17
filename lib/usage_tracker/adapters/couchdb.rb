cfac5d429fa610123ff0383ee4fcb67c7cedf3e2
Dmitriy Karpus <dmitriy.karpus@gmail.com>
2011-05-17 20:51:25 +0300

    Started work on storages abstraction

---
+++ lib/usage_tracker/adapters/couchdb.rb
@@ -0,0 +1,37 @@
+require 'pathname'
+require 'couchrest'
+require 'yaml'
+
+module UsageTracker
+  module Adapters
+    class Couchdb
+
+      def initialize (settings)
+        @database = CouchRest.database!(settings.couchdb)
+      end
+
+      private
+      # Loads CouchDB views from views.yml and verifies that
+      # they are loaded in the current instance, upgrading
+      # them if necessary.
+      def load_views!
+        new = YAML.load ERB.new(
+          Pathname.new(__FILE__).dirname.join('..', '..', 'config', 'views.yml').read
+        ).result
+
+        id  = new['_id']
+        old = @database.get id
+
+        if old['version'].to_i < new['version'].to_i
+          log "Upgrading Design Document #{id} to v#{new['version']}"
+          @database.delete_doc old
+          @database.save_doc new
+        end
+
+      rescue RestClient::ResourceNotFound
+        log "Creating Design Document #{id} v#{new['version']}"
+        @database.save_doc new
+      end
+    end
+  end
+end
